e33fa92431b06a16766bcad8c3255ada7cb361b6
shammi akter <57355238+shammi-akter@users.noreply.github.com>
2021-01-31 17:27:44 +0600

    Add files via upload

---
+++ registration1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>registration Page</title>
+</head>
+<body>
+
+<h1>My registration page </h1>
+<from>
+<table>
+<tr> <td>Frist Name: </td>
+<td><input type= "text" name="firstname"></td></tr>
+<br>
+<tr> <td> email: </td>
+<td><input type= "text" name="email"></td></tr>
+<br>
+<tr> <td> user  Name: </td>
+<td><input type= "text" name="username"></td></tr>
+<br>
+<tr> <td>Password: </td>
+<td><input type= "text" name="password"></td></tr>
+<br>
+<tr> <td>Confirm Password: </td>
+<td><input type= "text" name="confrimpassword"></td></tr>
+<br>
+<tr><td> Gender:</td>
+<tr><td><input type="radio" name="gender">Male</td></tr>
+<tr><td> <input type="radio" name="gender">Female</td></tr>
+<tr><td><input type="radio" name="gender">Other</td>
+</tr>
+<tr><td>Date of Birth</td>
+</tr>
+
+
+<tr>
+<td><input type="button" value="Submit"> <input type="button" value="Reset"></td>
+</tr>
+
+
+</table>
+</form>
+
+
+
+
+</body>
+</html>
